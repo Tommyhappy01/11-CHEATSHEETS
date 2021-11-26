c8e5bed4735226163456e3c4a54ad1dbac19ccd1
tommyhappy <tommyhappy01@gmail.com>
2021-11-26 13:02:38 +0300

    1

---
+++ Makefile
@@ -0,0 +1,56 @@
+SRC := $(wildcard *.tex)
+
+.PHONY: default
+default: all
+
+.PHONY: all
+all: logos figures cheatsheets handouts
+	mkdir -p ./build/
+	cp cheatsheets*.p* ./build/
+	cp handout-*.p* ./build/
+
+.PHONY: logos
+logos:
+	cd logos && python mpl-logos2.py && pdfcrop mpl-logo2.pdf mpl-logo2.pdf
+
+.PHONY: figures
+figures:
+	# generate the figures
+	cd scripts && for script in *.py; do echo $$script; python $$script; done
+	# crop the figures
+	cd figures && for figure in *.pdf; do echo $$figure; pdfcrop $$figure $$figure; done
+	# regenerate some figures that should not be cropped
+	cd scripts && python styles.py
+
+.PHONY: cheatsheets
+cheatsheets:
+	xelatex cheatsheets.tex
+	convert -density 150 cheatsheets.pdf -scene 1 cheatsheets.png
+
+.PHONY: handouts
+handouts:
+	xelatex handout-beginner.tex
+	xelatex handout-intermediate.tex
+	xelatex handout-tips.tex
+	convert -density 150 handout-tips.pdf handout-tips.png
+	convert -density 150 handout-beginner.pdf handout-beginner.png
+	convert -density 150 handout-intermediate.pdf handout-intermediate.png
+
+.PHONY: fonts
+fonts:
+	make -C fonts/
+
+.PHONY: clean
+clean: $(SRC)
+	latexmk -c $^
+	- rm -rf ./build/
+
+.PHONY: clean-all
+clean-all: clean
+	- rm ./logos/mpl-logo2.pdf
+	git clean -f -X ./figures/
+	git clean -f ./scripts/*.pdf
+
+.PHONY: requirements
+requirements:
+	$(MAKE) -C ./requirements/
---
+++ scripts/layouts.py
@@ -0,0 +1,85 @@
+# -----------------------------------------------------------------------------
+# Matplotlib cheat sheet
+# Released under the BSD License
+# -----------------------------------------------------------------------------
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+fig = plt.figure(figsize=(0.4,0.4))
+margin = 0.01
+fig.subplots_adjust(left=margin, right=1-margin, top=1-margin, bottom=margin)
+mpl.rc('axes', linewidth=.5)
+
+# Subplots
+# -----------------------------------------------------------------------------
+nrows, ncols = 3,3
+for i in range(nrows*ncols):
+    ax = plt.subplot(ncols, nrows, i+1)
+    ax.set_xticks([]), ax.set_yticks([])
+plt.savefig("../figures/layout-subplot.pdf")
+fig.clear()
+
+# Subplots (colored)
+# -----------------------------------------------------------------------------
+nrows, ncols = 3,3
+for i in range(nrows*ncols):
+    ax = plt.subplot(ncols, nrows, i+1)
+    ax.set_xticks([]), ax.set_yticks([])
+    if i == 0: ax.set_facecolor("#ddddff")
+    if i == 8: ax.set_facecolor("#ffdddd")
+plt.savefig("../figures/layout-subplot-color.pdf")
+fig.clear()
+
+# Spines
+# -----------------------------------------------------------------------------
+ax = fig.add_subplot(1,1,1, xticks=[], yticks=[])
+ax.spines["top"].set_color("None")
+ax.spines["right"].set_color("None")
+plt.savefig("../figures/layout-spines.pdf")
+fig.clear()
+
+
+# Gridspec
+# -----------------------------------------------------------------------------
+gs = fig.add_gridspec(3, 3)
+ax1 = fig.add_subplot(gs[0, :], xticks=[], yticks=[])
+ax2 = fig.add_subplot(gs[1, :-1], xticks=[], yticks=[])
+ax3 = fig.add_subplot(gs[1:, -1], xticks=[], yticks=[])
+ax4 = fig.add_subplot(gs[-1, 0], xticks=[], yticks=[])
+ax5 = fig.add_subplot(gs[-1, -2], xticks=[], yticks=[])
+plt.savefig("../figures/layout-gridspec.pdf")
+fig.clear()
+
+# Gridspec (colored)
+# -----------------------------------------------------------------------------
+gs = fig.add_gridspec(3, 3)
+ax1 = fig.add_subplot(gs[0, :], xticks=[], yticks=[])
+ax1.set_facecolor("#ddddff")
+ax2 = fig.add_subplot(gs[1, :-1], xticks=[], yticks=[])
+ax3 = fig.add_subplot(gs[1:, -1], xticks=[], yticks=[])
+ax4 = fig.add_subplot(gs[-1, 0], xticks=[], yticks=[])
+ax5 = fig.add_subplot(gs[-1, -2], xticks=[], yticks=[])
+plt.savefig("../figures/layout-gridspec-color.pdf")
+fig.clear()
+
+# Inset axes
+# -----------------------------------------------------------------------------
+mpl.rc('axes', linewidth=.5)
+margin = 0.0125
+ax1 = fig.add_axes([margin,margin,1-2*margin,1-2*margin], xticks=[], yticks=[])
+ax2 = ax1.inset_axes([0.5, 0.5, 0.4, 0.4], xticks=[], yticks=[])
+plt.savefig("../figures/layout-inset.pdf")
+fig.clear()
+
+
+# Axes divider
+# -----------------------------------------------------------------------------
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+margin = 0.0125
+ax = fig.add_axes([margin,margin,1-2*margin,1-2*margin], xticks=[], yticks=[])
+divider = make_axes_locatable(ax)
+cax = divider.new_horizontal(size="10%", pad=0.025)
+fig.add_axes(cax)
+cax.set_xticks([]), cax.set_yticks([])
+plt.savefig("../figures/layout-divider.pdf")
